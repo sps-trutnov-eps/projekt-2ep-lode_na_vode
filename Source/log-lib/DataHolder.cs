e1dde0cbb3d44ca4c9215a8f0de6d0960d7cf603
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2023-04-11 13:15:42 +0200

    Získání hlášek lodi
    zatím neotestované

--- Source/log-lib/DataHolder.cs
+++ Source/log-lib/DataHolder.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace log_lib {
-    public struct Lod {
+    public struct Lod { 
         string jmeno;
-        string[] hlasky;
+        string[] hitHlasky;
+        string[] moveHlasky;
+        public Lod(string jmeno, string[] hit, string[] move)
+        {
+            this.jmeno = jmeno;
+            hitHlasky = hit;
+            moveHlasky = move;
+        }
     }
-
     public struct Hrac {
         public string jmeno;
         public string barva;
@@ -21,14 +28,30 @@ namespace log_lib {
         List<Hrac> Hraci;
         List<string> nalepky;
 
+        StreamReader hlaskySR;
+        StreamReader nalepkySR;
+
         public DataHolder(string cestakLodim,string cestaKNalepkam){
             Lode = new List<Lod>();
             Hraci = new List<Hrac>();
-        }
-        public string[] GetHlasky(string jmenoLodi) {
+            StreamReader hlaskySR = new StreamReader(cestakLodim);
+            StreamReader nalepkySR = new StreamReader(cestaKNalepkam);
 
         }
-
+        public Lod GetHlasky(string jmenoLodi) {
+            string jmeno = jmenoLodi;
+            string[] hitHlasky = new string[10];
+            string[] moveHlasky = new string[10];
+            while (hlaskySR.ReadLine() != null) {
+                if (hlaskySR.ReadLine() == jmenoLodi)
+                {
+                    hitHlasky = hlaskySR.ReadLine().Split(";");
+                    moveHlasky = hlaskySR.ReadLine().Split(";");
+                }
+            }
+            Lod lod = new Lod(jmenoLodi, hitHlasky, moveHlasky);
+            return lod;
+        }
         public string GetHracBarva(string jmenoHrace) {
 
         }

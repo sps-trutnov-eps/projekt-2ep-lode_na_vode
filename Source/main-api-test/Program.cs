5d57950c11571bcfafd1eecf9a065326bfc0c2e8
Jakub_Polák <polakj21@spstrutnov.cz>
2023-04-11 13:20:51 +0200

    template

---
+++ Source/main-api-test/Program.cs
@@ -0,0 +1,9 @@
+﻿using main_api;
+
+namespace main_api_test {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Source/main-api/Engine.cs
@@ -0,0 +1,36 @@
+﻿using log_lib;
+
+namespace main_api {
+    public class Engine {
+
+        Log GetLog;
+
+        public Engine() {
+            GetLog = new Log();
+
+        }
+
+        public void Strelba(int x, int y) { 
+
+        }
+
+        public void UmistitLod(int x, int y) { 
+
+        }
+
+        public void PohybLode() {
+
+        }
+
+        public void DalseHrac() {
+
+        }
+        
+
+
+
+
+
+
+    }
+}

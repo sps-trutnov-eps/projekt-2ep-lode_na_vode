9828882bd4a5c857597cb1da4be559ea68a8f1a3
Jakub_Polák <polakj21@spstrutnov.cz>
2023-04-11 12:41:36 +0200

    template logu
    neznám česky

---
+++ Source/log-lib/DataHolder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log_lib {
+    public struct Lod {
+        string jmeno;
+        string[] hlasky;
+    }
+
+    public struct Hrac {
+        public string jmeno;
+        public string barva;
+    }
+
+    internal class DataHolder {
+
+        List<Lod> Lode;
+        List<Hrac> Hraci;
+        List<string> nalepky;
+
+        public DataHolder(string cestakLodim,string cestaKNalepkam){
+            Lode = new List<Lod>();
+            Hraci = new List<Hrac>();
+        }
+        public string[] GetHlasky(string jmenoLodi) {
+
+        }
+
+        public string GetHracBarva(string jmenoHrace) {
+
+        }
+
+    }
+}
---
+++ Source/log-lib/Log.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace log_lib {
+    public class Log {
+
+        private List<string> EntireLog;
+        private DataHolder Lode;
+
+
+        public Log(string cestaKLodim, string cestaKNalepkam) {
+            EntireLog = new List<string>();
+            Lode = new DataHolder(cestaKLodim,cestaKNalepkam);
+
+        }
+
+        public string GetHitMessage(string jmenoHrace, string jmenoLodi) {
+
+        }
+
+        public string[] GetEntireLog() {
+
+        }
+
+        public string GetLodMovement(string jmenoHrace, string jmenoLodi) {
+
+        }
+
+        public string MissStreak(string jmenoHrace, ushort pocetMisu) {
+
+        }
+
+        public string[] GetNalepky() {
+
+        }
+
+        public string ActivateNalepka(string jmenoHrace, string Nalepka) {
+
+        }
+
+    }
+}

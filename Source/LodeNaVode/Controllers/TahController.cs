4ce37ea7cdd30087f476e8b38eb9e9a2568d6903
Marek Langer <langerm21@spstrutnov.cz>
2023-04-17 11:17:23 +0200

    Typy policek

--- Source/LodeNaVode/Controllers/TahController.cs
+++ Source/LodeNaVode/Controllers/TahController.cs
@@ -1,15 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace LodeNaVode.Controllers
 {
+    public enum TypPolicka
+    {
+        Lod,
+        Voda,
+        ZasahMimo,
+        ZasahLod,
+        LodPotopena,
+    }
     public class TahController : Controller
     {
+
+
         public IActionResult Policko(int id)
         {
+            TypPolicka[,] bojiste = new TypPolicka[15, 15];
+
+            for(int y = 0; y < bojiste.GetLength(0); y++)
+            {
+                for(int x = 0; x < bojiste.GetLength(1); x++)
+                {
+                    bojiste[y, x] = TypPolicka.Voda;
+                }
+            }
+            bojiste[6, 10] = TypPolicka.Lod;
+            bojiste[4, 6] = TypPolicka.LodPotopena;
+            bojiste[0, 14] = TypPolicka.ZasahMimo;
+            bojiste[14, 1] = TypPolicka.ZasahLod;
+
+
             Debug.WriteLine(id);
 
-            return View();
+            return View(bojiste);
         }
     }
 }

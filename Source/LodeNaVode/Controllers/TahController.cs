2ea971d5e604d28659e6cb901e172677503c9fd9
Karel Kříž <KrizK21@spstrutnov.cz>
2023-04-27 14:20:54 +0200

    Kód přečten, příprava na mechaniky

--- Source/LodeNaVode/Controllers/TahController.cs
+++ Source/LodeNaVode/Controllers/TahController.cs
@@ -11,6 +11,7 @@ namespace LodeNaVode.Controllers
         ZasahMimo,
         ZasahLod,
         LodPotopena,
+        Mlha,
     }
     public class TahController : Controller
     {
@@ -18,20 +19,33 @@ namespace LodeNaVode.Controllers
 
         public IActionResult Policko(int id)
         {
-            TypPolicka[,] bojiste = new TypPolicka[15, 15];
+            TypPolicka[,] bojiste = new TypPolicka[15, 11];
 
             for(int y = 0; y < bojiste.GetLength(0); y++)
             {
                 for(int x = 0; x < bojiste.GetLength(1); x++)
                 {
-                    bojiste[y, x] = TypPolicka.Voda;
+                    bojiste[y, x] = TypPolicka.Mlha;
                 }
             }
             bojiste[6, 10] = TypPolicka.Lod;
             bojiste[4, 6] = TypPolicka.LodPotopena;
-            bojiste[0, 14] = TypPolicka.ZasahMimo;
+            bojiste[0, 10] = TypPolicka.ZasahMimo;
             bojiste[14, 1] = TypPolicka.ZasahLod;
 
+            int cislo = 0;
+            for(int y = 0; y < bojiste.GetLength(0); y++)
+            {
+                for (int x = 0; x < bojiste.GetLength(1); x++, cislo++)
+                {
+                    if (id == cislo)
+                    {
+                        // mechanika sem, např: pokud klikne na mlhu tak vystřel
+                        
+                        //bojiste[y, x] ==
+                    }
+                }
+            }
 
             Debug.WriteLine(id);
 

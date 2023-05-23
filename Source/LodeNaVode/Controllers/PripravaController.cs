71398e790003aeeb4404ede94dc5b7756be226de
Jan Štěpánek <stepanekj21@spstrutnov.cz>
2023-05-23 13:21:27 +0200

    Třeba to někdy bude fungovat

---
+++ Source/LodeNaVode/Controllers/PripravaController.cs
@@ -0,0 +1,19 @@
+﻿using LodeNaVode.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LodeNaVode.Controllers
+{
+
+    public class PripravaController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Zvolit()
+        {
+            return View();
+        }
+
+    }
+
+}

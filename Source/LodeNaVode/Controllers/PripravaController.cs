39b276c64642cbd79c6c8ddafac4a55edfb8012f
Jan Štěpánek <stepanekj21@spstrutnov.cz>
2023-05-25 14:51:55 +0200

    Něco málo

--- Source/LodeNaVode/Controllers/PripravaController.cs
+++ Source/LodeNaVode/Controllers/PripravaController.cs
@@ -7,11 +7,22 @@ namespace LodeNaVode.Controllers
 
     public class PripravaController : Controller
     {
+        public float pocetLodi = 0;
 
         [HttpGet]
         public IActionResult Zvolit()
         {
-            return View();
+            Debug.WriteLine("Zvolit");
+            Debug.WriteLine(pocetLodi);
+            return View(pocetLodi);
+        }
+
+        public IActionResult KliknutiPlus()
+        {
+            Debug.WriteLine("KliknutiPlus");
+            pocetLodi ++;
+            Debug.WriteLine(pocetLodi);
+            return View("Zvolit", pocetLodi);
         }
 
     }

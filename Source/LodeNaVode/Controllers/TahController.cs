094d6c01184077e4f105821d75aab5f6ed64db0a
Karel Kříž <KrizK21@spstrutnov.cz>
2023-06-06 12:17:50 +0200

    ovládání (beta)

--- Source/LodeNaVode/Controllers/TahController.cs
+++ Source/LodeNaVode/Controllers/TahController.cs
@@ -13,6 +13,7 @@ namespace LodeNaVode.Controllers
     public enum TypPolicka
     {
         Lod,
+        VedlejsiLod,
         NepratelskaLod,
         Voda,
         ZasahLod,
@@ -40,26 +41,10 @@ namespace LodeNaVode.Controllers
         }
     }
 
-    public static class Paměť
+    public static class Pamet
     {
-
-        public static int lodPolicko = -1;
-
-        private static Engine GetIT()
-        {
-            string[][] mojeStringy = new string[][] {
-                new string[] { "a", "b" },
-                new string[] { "c", "d" }
-            };
-            Engine engine = new Engine(mojeStringy, 10, 15, "../../Data/textury/tvary-lodi.TEXT", "LodeNaVode/Lode/hlasky.txt", "LodeNaVode/Lode/nalepky.txt");
-
-            engine.UmistitLod(2, 5, "L", "a", "d");
-
-            Debug.WriteLine("hi");
-
-            return engine;
-
-        }
+        public static int lodId = -1;
+        public static bool oznacenaLod = false;
     }
 
     public class TahController : Controller
@@ -84,10 +69,12 @@ namespace LodeNaVode.Controllers
 
         public IActionResult Policko(int id)
         {
+
             Debug.WriteLine(id);
 
             Engine engine = Engin.engine;
-            ref int lodId = ref Paměť.lodPolicko;
+            ref int lodId = ref Pamet.lodId;
+            ref bool oznacenaLod = ref Pamet.oznacenaLod;
 
             TypPolicka[,] bojiste = new TypPolicka[15, 10];
 
@@ -96,6 +83,40 @@ namespace LodeNaVode.Controllers
             bojiste[2, 6] = TypPolicka.NepratelskaLod;
 
             int cislo = 0;
+            
+
+            // Pohyb lodě
+            // Negativní id se používá jako ovládání
+            // -1 == null
+            // -2 otočit levá
+            // -3 hore
+            // -4 otočit pravá
+            // -5 levá
+            // -6 dolů
+            // -7 pravá
+            if(oznacenaLod) {
+                oznacenaLod = false;
+                switch(id)
+                {
+                    case -2:
+                        engine.OtoceniVlevo(lodId);
+                        break;
+
+                    case -3:
+                        engine.OtoceniVpravo(lodId);
+                        break;
+
+                    case -4:
+                        engine.PohybLode(lodId, "sever");
+                        break;
+
+                    case -5:
+                        engine.PohybLode(lodId, "jih");
+                        break;
+                }
+            }
+
+            // Procházení bojištěm
             for (int y = 0; y < bojiste.GetLength(0); y++)
             {
                 for (int x = 0; x < bojiste.GetLength(1); x++, cislo++)
@@ -111,10 +132,22 @@ namespace LodeNaVode.Controllers
 
                         if (policko == TypPolicka.Lod)
                         {
-                            lodId = cislo;
-                            Debug.WriteLine(lodId);
+                            for (int i = 0; i < engine.Lode.Count; i++)
+                            {
+                                var lod = engine.Lode[i];
+
+                                if (lod.CentralneBod[0] == x && lod.CentralneBod[1] == y)
+                                {
+                                    lodId = i;
+                                    oznacenaLod = true;
+                                }
+                            }
+                            Debug.WriteLine("LodID: " + lodId);
                             //engine.PohybLode(0, "jih");
-                            engine.PohybLode(0, "sever");
+                        }
+                        else
+                        {
+                            oznacenaLod = false;
                         }
 
                         /*if (policko == TypPolicka.Mlha)
@@ -129,8 +162,6 @@ namespace LodeNaVode.Controllers
             }
 
             Redraw(ref bojiste, ref engine);
-
-            
 
             return View(bojiste);
         }
